fcb375306b2349946f91e8687e3e5e6322d851c7
Rowida Hany <rowidahany084@gmail.com>
2026-08-06 15:32:00 +0300

    Refactor clsCompany class and remove Program class

--- services/clsCompany.cs
+++ services/clsCompany.cs
@@ -1,285 +1,232 @@
 using ConsoleApp4.Models;
-using ConsoleApp4.Services;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp4.Services
 {
-
-    internal class Program
+    internal class clsCompany
     {
-        private enum enOption
+        public Queue<clsEmployee> onboarding=new Queue<clsEmployee>();
+        public Stack<clsEmployee> actionHistory=new Stack<clsEmployee>();
+        public List<clsEmployee> activeEmployees=new List<clsEmployee>();
+        public Dictionary<int, string> departments=new Dictionary<int, string>();
+        public HashSet<string> uniqueSkills=new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        public bool IDIsFound(int id )
         {
-            enaddEmployeeOnboarding = 1, enaddDeparment = 3, enProcessNextEmployee = 2,
-            enSearchEmpByID = 4, enSearchEmpByName = 5, enPrintEmps = 6, enCalcSalaryAvrage = 7,
-            enEmpsReport = 8, enPrintActionHistory = 9, enPrintUniqueSkills = 10,enExite=0
+            foreach (clsEmployee emp in activeEmployees)
+            {
+                if (emp.ID == id)
+                {
+                    return true;
+                }
+            }
+             foreach (clsEmployee emp in onboarding)
+    {
+        if (emp.ID == id)
+            return true;
+    }
+
+            return false;
         }
 
-        static private clsEmployee _readEmployee(clsCompany company)
+        public bool departmentNameIsFound(string departmentName)
         {
-            Console.Write("Enter Name : ");
-            string name = Console.ReadLine();
 
-            Console.Write("Enter ID : ");
-            int id = int.Parse(Console.ReadLine());
-
-            while (company.IDIsFound(id))
+            foreach (KeyValuePair<int, string> department in departments)
             {
-                Console.WriteLine("This ID is Already Exists!");
-                Console.Write("Enter ID : ");
-                id = int.Parse(Console.ReadLine());
+                if (department.Value.Equals(departmentName,StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
             }
-
-
-            Console.Write("Enter DepartmentID : ");
-            int departmentID = int.Parse(Console.ReadLine());
-            Console.Write("Unique Skill : ");
-            string uniqueSkill = Console.ReadLine();
-            Console.Write("Enter Salary : ");
-            double salary = double.Parse(Console.ReadLine());
-            clsEmployee emp = new clsEmployee(name, id, departmentID, salary, uniqueSkill);
-            return emp;
+            return false;
         }
 
-        static void SeedData(clsCompany company)
+        public bool depatrmentIDIsFound(int departmentID)
         {
-
-            company.addDepartment(1, "HR");
-            company.addDepartment(2, "IT");
-            company.addDepartment(3, "Finance");
-            company.addDepartment(4, "Marketing");
-            company.addDepartment(5, "Sales");
-            company.addDepartment(6, "Customer Support");
-
-
-            clsEmployee[] employees =
-            {
-        new clsEmployee("Ahmed Hassan",     101, 2, 12000, "C#"),
-        new clsEmployee("Sara Ali",         102, 1,  8000, "Recruitment"),
-        new clsEmployee("Omar Mohamed",     103, 3, 11000, "Excel"),
-        new clsEmployee("Mona Adel",        104, 2, 15000, "ASP.NET"),
-        new clsEmployee("Youssef Samy",     105, 5,  9000, "Negotiation"),
-        new clsEmployee("Nour Ahmed",       106, 2, 13000, "SQL"),
-        new clsEmployee("Khaled Tarek",     107, 4, 10000, "Photoshop"),
-        new clsEmployee("Salma Mostafa",    108, 3, 14000, "Power BI"),
-        new clsEmployee("Hana Ibrahim",     109, 6,  7500, "Customer Service"),
-        new clsEmployee("Karim Magdy",      110, 2, 16000, "C#"),
-        new clsEmployee("Mariam Hany",      111, 4, 11500, "Content Writing"),
-        new clsEmployee("Amr Essam",        112, 5, 10500, "Sales"),
-        new clsEmployee("Aya Nabil",        113, 1,  8500, "Communication"),
-        new clsEmployee("Mahmoud Adel",     114, 2, 17000, "SQL"),
-        new clsEmployee("Reem Wael",        115, 6,  7800, "Problem Solving"),
-        new clsEmployee("Mostafa Ashraf",   116, 2, 14500, "ASP.NET"),
-        new clsEmployee("Laila Sherif",     117, 3, 13500, "Excel"),
-        new clsEmployee("Mohamed Gamal",    118, 2, 18000, "C#"),
-        new clsEmployee("Farah Khaled",     119, 4,  9800, "Photoshop"),
-        new clsEmployee("Ziad Hassan",      120, 5, 12500, "Negotiation")
-    };
-
-
-            foreach (clsEmployee emp in employees)
-            {
-                company.addOnboardingEmployee(emp);
-                company.addSkill(emp);
-            }
-
-            for (int i = 0; i < 15; i++)
-            {
-                company.addActionEmployee();
-            }
+            return departments.ContainsKey(departmentID);
         }
-        static private void _mainMenuPerformance(enOption choice, clsCompany company)
+        public bool addOnboardingEmployee(clsEmployee newEmp)
         {
-            switch (choice)
+            if (IDIsFound(newEmp.ID))
+                return false;
+
+            onboarding.Enqueue(newEmp);
+            actionHistory.Push(newEmp);
+            return true;
+        }
+
+        public bool addActionEmployee()
+        {
+            if (onboarding.Count > 0)
             {
-                case enOption.enaddEmployeeOnboarding:
-                    {
-                        clsEmployee emp = _readEmployee(company);
-                        if (company.addOnboardingEmployee(emp))
-                        {
-                            company.addSkill(emp);
-                            Console.WriteLine("This Employee Is Added Successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("This ID Is Already Exist!");
-                        }
-                        break;
-                    }
+                clsEmployee employee = onboarding.Dequeue();
+                activeEmployees.Add(employee);
+                return true;
+            }
 
-                case enOption.enaddDeparment:
-                    {
-                        Console.Write("Enter Department Name : ");
-                        string departmentName = Console.ReadLine();
-                        while (company.departmentNameIsFound(departmentName))
-                        {
-                            Console.WriteLine("This Name Of Department is Already Exists!");
-                            Console.Write("Enter Another Department Name : ");
-                            departmentName = Console.ReadLine();
-                        }
-                        Console.Write("Enter Department ID : ");
-                        int id = int.Parse(Console.ReadLine());
-                        while (company.depatrmentIDIsFound(id))
-                        {
-                            Console.WriteLine("This ID Of Department is Already Exists!");
-                            Console.Write("Enter Department ID : ");
-                            id = int.Parse(Console.ReadLine());
-                        }
+            return false;
+        }
 
-                        if (company.addDepartment(id, departmentName))
-                        {
-                            Console.WriteLine("Department Added Successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("This Department Already Exists!");
-                        }
-                        Console.WriteLine();
-                        break;
-                    }
+        public bool addDepartment(int ID, string name)
+        {
+            if (!departments.ContainsKey(ID))
+            {
+                departments[ID] = name;
+                return true;
+            }
+            return false;
+        }
 
-                case enOption.enProcessNextEmployee:
-                    {
-                        if (company.addActionEmployee())
-                        {
-                            Console.WriteLine("Employee Added Successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Employee Failed To Be Added!");
-                        }
-                        Console.WriteLine();
-                        break;
-                    }
+        public void addSkill(clsEmployee emp)
+        {
+            uniqueSkills.Add(emp.uniqueSkill);
+        }
 
-                case enOption.enSearchEmpByID:
-                    {
-                        Console.Write("Enter Employee ID : ");
-                        int id = int.Parse(Console.ReadLine());
-                        clsEmployee emp = company.searchEmployeeByID(id);
-                        if (emp != null)
-                        {
-                            emp.Print();
-                        }
-                        else
-                        {
-                            Console.WriteLine("This Employee Does Not Exist!");
-                        }
-                        Console.WriteLine();
-                        break;
-                    }
+        public clsEmployee searchEmployeeByID(int ID)
+        {
+            foreach (clsEmployee employee in activeEmployees)
+            {
+                if (employee.ID == ID)
+                {
+                    return employee;
+                }
+            }
+            return null;
+        }
 
-                case enOption.enSearchEmpByName:
-                    {
-                        Console.Write("Enter Employee Name : ");
-                        string empName = Console.ReadLine();
-                        clsEmployee emp = company.searchEmployeeByName(empName);
-                        if (emp != null)
-                        {
-                            emp.Print();
-                        }
-                        else
-                        {
-                            Console.WriteLine("This Employee Does Not Exist!");
-                        }
-                        Console.WriteLine();
-                        break;
-                    }
+        public clsEmployee searchEmployeeByName(string name)
+        {
+            foreach(clsEmployee employee in activeEmployees)
+            {
+                if(employee.name.Equals(name,StringComparison.OrdinalIgnoreCase))
+                {
+                    return (employee);
+                }
+            }
+            return null;
+        }
 
-                case enOption.enPrintEmps:
-                    {
-                        Console.Write("Enter Department Name : ");
-                        string departmentName = Console.ReadLine();
+        private int _getDepartmentIDByName(string departmentName)
+        {
+            
+            foreach(KeyValuePair<int,string>item in departments)
+            {
+                if(item.Value.Equals(departmentName,StringComparison.OrdinalIgnoreCase))
+                {
+                    return  item.Key;
+                }
+            }
+            return -1;
+        }
 
-                        if (!company.printAllEmployeeByDepartment(departmentName))
-                            Console.WriteLine("This Department Does Not Have Any Employee");
-                        Console.WriteLine();
-                        break;
+        public bool printAllEmployeeByDepartment(string department)
+        {
+            int departmentID = _getDepartmentIDByName(department);
+            if (departmentID == -1)
+                return false;
+            if (_hasEmployee(department))
+            {
+                foreach (clsEmployee employee in activeEmployees)
+                {
+                    if (employee.departmentID == departmentID)
+                    {
+                        employee.Print();
                     }
+                }
+                return true;    
+            }
+            return false;
+         
+        }
 
-                case enOption.enCalcSalaryAvrage:
-                    {
-                        Console.WriteLine($"Salary Average : {company.calcSalaryAverage()}");
-                        Console.WriteLine();
-                        break;
-                    }
+        public double calcSalaryAverage()
+        {
+            double sum = 0;
+            foreach(clsEmployee employee in activeEmployees)
+            {
+                sum += employee.salary;
+            }
+            if(activeEmployees.Count ==0)
+            {
+                return 0;
+            }
+            return sum / activeEmployees.Count;
+        }
 
-                case enOption.enEmpsReport:
-                    {
-                        company.numOfEmployeesInEachDepartmentReport();
-                        Console.WriteLine();
-                        break;
-                    }
+        private  Dictionary<int,int> _getNumberOfEmployeeInEachDepartment()
+        {
+            Dictionary<int, int> numOfEmployees=new Dictionary<int, int>();
+            foreach(clsEmployee employee in activeEmployees)
+            {
+                if (numOfEmployees.ContainsKey(employee.departmentID))
+                {
+                    numOfEmployees[employee.departmentID]++;
+                }
+                else
+                {
+                    numOfEmployees[employee.departmentID] = 1;
+                }
+            }
+            return numOfEmployees;
+        }
 
-                case enOption.enPrintActionHistory:
-                    {
-                        company.printActionHistory();
-                        Console.WriteLine();
-                        break;
-                    }
-                case enOption.enPrintUniqueSkills:
-                    {
-                        company.printUniqueSkills();
-                        Console.WriteLine();
-                        break;
-                    }
-                case enOption.enExite:
-                    {
-                        return;
-                    }
-                default:
-                    Console.WriteLine("Invalid Choice");
-                    break;
+        private bool _hasEmployee(string departmentName)
+        {
+            int departmentID=_getDepartmentIDByName(departmentName);
+            foreach (clsEmployee employee in activeEmployees)
+            {
+               if(employee.departmentID==departmentID)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void numOfEmployeesInEachDepartmentReport()
+        {
+            Dictionary<int, int> numOfEmployees=_getNumberOfEmployeeInEachDepartment();
+            foreach(KeyValuePair<int,string> item in departments)
+            {
+                if (numOfEmployees.ContainsKey(item.Key))
+                {
+                    Console.WriteLine($"{item.Value}: {numOfEmployees[item.Key]}");
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Value}: 0");
+                }
             }
         }
 
-        static private short _readChoice()
+        public void printActionHistory()
         {
-            Console.Write("Enter Your Choice: ");
-            short choice=short.Parse(Console.ReadLine());
-            while(choice < 0 || choice > 10)
+           Stack <clsEmployee>tempEmpsActions = new Stack<clsEmployee>();
+            while(actionHistory.Count > 0)
             {
-                Console.WriteLine("Inavailable Option!");
-                Console.Write("Enter Another Choice: ");
-                choice = short.Parse(Console.ReadLine());
+                clsEmployee emp=actionHistory.Pop();
+                emp.Print();
+                Console.WriteLine("=========================================");
+                tempEmpsActions.Push(emp);
             }
-            return choice;
-        }
-        static void Main(string[] args)
-        {
 
-            clsCompany company = new clsCompany();
-            SeedData(company);
-            short choice;
-
-            do
+            while(tempEmpsActions.Count > 0)
             {
-                Console.WriteLine("========== Company Management ==========");
-                Console.WriteLine("1. Add Employee To Onboarding");
-                Console.WriteLine("2. Process Next Employee");
-                Console.WriteLine("3. Add Department");
-                Console.WriteLine("4. Search Employee By ID");
-                Console.WriteLine("5. Search Employee By Name");
-                Console.WriteLine("6. Print Employees By Department");
-                Console.WriteLine("7. Calculate Salary Average");
-                Console.WriteLine("8. Employees Report By Department");
-                Console.WriteLine("9. Print Action History");
-                Console.WriteLine("10.Print Unique Skills");
-                Console.WriteLine("0. Exit");
-                Console.WriteLine();
-                Console.Write("\nEnter your choice: ");
-                choice = _readChoice();
+                clsEmployee temp=tempEmpsActions.Pop();
+                actionHistory.Push(temp);
+            }
+        }
 
-                Console.WriteLine();
-                Console.WriteLine();
-                _mainMenuPerformance((enOption)choice, company);
-            } while (choice != 0);
-
-
+        public void printUniqueSkills()
+        {
+            foreach(string item in  uniqueSkills)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
